5d113819136560a2f231c0b6c7238a0718c872de
noyandogane <ltmpinfo@gmail.com>
2022-10-20 17:03:19 +0300

    Bug Fix

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const anaSayfa= function(req, res) {
-    res.render('anaSayfa', 
+    res.render('anasayfa', 
     {
         'baslik': 'Ana sayfa',
         'sayfaBaslik':{
