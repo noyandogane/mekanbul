ff68089b0fba98304f4543fc5b06a2b34bc7c290
fromagery <66711531+fromagery@users.noreply.github.com>
2022-10-26 20:20:50 +0300

    Fix bugs

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -30,7 +30,7 @@ const anaSayfa= function(req, res) {
     });
 }
 const mekanBilgisi= function(req, res) {
-    res.render("mekansilgisi",
+    res.render("mekanbilgisi",
      {
         "baslik": "Mekan bilgisi",
         "mekanBaslik":"Starbucks",
