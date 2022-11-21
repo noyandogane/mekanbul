d48f43598fa4ce6c5f2fa864a19d8746be748ad8
fromagery <ltmpinfo@gmail.com>
2022-11-22 00:59:01 +0300

    odev5
    database migration

--- app.js
+++ app.js
@@ -3,39 +3,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require("./app_api/models/db");
 var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-
-require('./app_server/models/db');
+var usersRouter = require('./app_server/routes/users'); 
+var apiRouter=require('./app_api/routes/index');
 
 var app = express();
+app.use("/api",apiRouter);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('views', path.join(__dirname, 'app_server','views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
