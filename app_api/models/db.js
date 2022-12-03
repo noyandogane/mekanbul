33436b4ba31571ae993fc07cac452babd9f34f8b
noyandogane <ltmpinfo@gmail.com>
2022-12-03 10:32:06 +0300

    Update db.js

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -2,7 +2,7 @@ var mongoose=require("mongoose");
 require("./mekansema");
 var dbURI = "mongodb+srv://noyan:1234@mekanbul.46wmrds.mongodb.net/mekanbul?retryWrites=true&w=majority"; 
 //var dbURI = "mongodb://localhost/mekanbul";
-mongoose.connect(dbURI, {useNewUrlParser:true});
+mongoose.connect(dbURI);
 function kapat(msg, callback) {
     mongoose.connection.close(function () {
         console.log(msg);
