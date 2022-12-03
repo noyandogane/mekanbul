02cd9a54f36f035aef192cd84a22e40e1ae7c9ba
fromagery <ltmpinfo@gmail.com>
2022-12-03 07:30:08 +0300

    odev7

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,71 +1,100 @@
+const axios = require("axios");
+var apiSecenekleri = {
+    sunucu: "http://localhost:3000",
+    apiYolu: "/api/mekanlar/",
+};
+var mesafeyiFormatla = function (mesafe) {
+    var yeniMesafe, birim;
+    if (mesafe > 1) {
+        yeniMesafe = parseFloat(mesafe).toFixed(1);
+        birim = "km";
+    } else {
+        yeniMesafe = parseInt(mesafe * 1000, 10);
+        birim = "m";
+    }
+    return yeniMesafe + birim;
+};
 var express = require('express');
 var router = express.Router();
 
-const anaSayfa= function(req, res) {
-    res.render('anasayfa', 
-    {
-        'baslik': 'Ana sayfa',
-        'sayfaBaslik':{
-            'siteAd':'MekanBul',
-            'slogan':'Civardaki Mekanları Keşfet!'
+var anasayfaOlustur = function (res, mekanListesi) {
+    var mesaj;
+    if (!(mekanListesi instanceof Array)) {
+        mesaj = "API HATASI: Birşeyler ters gitti";
+        mekanListesi = [];
+    } else {
+        if (!mekanListesi.length) {
+            mesaj = "Civarda herhangi bir mekan bulunamadı!";
+        }
+    }
+    res.render('anasayfa', {
+        baslik: 'MekanBul',
+        sayfaBaslik: {
+            siteAd: 'MekanBul',
+            aciklama: 'Civardaki mekanları keşfedin!'
         },
-        'Mekanlar':[
-            {
-                "ad":"Starbucks",
-                "puan":"4",
-                "adres":"Centrum Garden AVM",
-                "imkanlar":["Dünya Kahveleri","Kekler","Pastalar"],
-                "mesafe":"15 km"
-            },
-            {
-                "ad":"Alina Sushi",
-                "puan":"4",
-                "adres":"Centrum Garden AVM",
-                "imkanlar":["Sushi","Nigiri","Maki"],
-                "mesafe":"15 km"
+        mekanlar: mekanListesi,
+        mesaj: mesaj
+    });
+};
+
+const anaSayfa= function(req, res) {
+    axios.get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu, {
+        params: {
+            enlem: req.query.enlem,
+            boylam: req.query.boylam
+        }
+        })
+        .then(function (response) {
+            var i, mekanlar;
+            mekanlar = response.data;
+            for (i = 0; i < mekanlar.length; i++) {
+                mekanlar[i].mesafe = mesafeyiFormatla(mekanlar[i].mesafe);
             }
-            
-        ]
-        
+            anasayfaOlustur(res, mekanlar);
+        })
+        .catch(function (hata) {
+            anasayfaOlustur(res, hata);
+        });   
+}
+
+var detaySayfasiOlustur = function (res, mekanDetaylari) {
+    mekanDetaylari.koordinat={
+        "enlem":mekanDetaylari.koordinat[0],
+        "boylam":mekanDetaylari.koordinat[1]
+    };
+    res.render('mekanbilgisi', {
+        mekanBaslik: mekanDetaylari.ad,
+        mekanDetay: mekanDetaylari
     });
 }
-const mekanBilgisi= function(req, res) {
-    res.render("mekanbilgisi",
-     {
-        "baslik": "Mekan bilgisi",
-        "mekanBaslik":"Starbucks",
-        "mekanDetay": {
-            "ad":"Starbucks",
-            "adres":"Centrum Garden AVM",
-            "puan":"4",
-            "imkanlar":["Dünya Kahveleri","Kekler","Pastalar"],
-            "koordinatlar":{
-                "enlem":"37.78",
-                "boylam":"30.56"
-        },
-        "saatler":[{
-                "gunler":"Pazartesi-Cuma",
-                "acilis":"09:00",
-                "kapanis":"23:00",
-                "kapali": false
-            },
-            {
-                "gunler":"Cumartesi-Pazar",
-                "acilis":"10:00",
-                "kapanis":"22:00",
-                "kapali": false
-            }
-        ],
-        "yorumlar":[
-            {
-                "yorumYapan":"Noyan Doğan Enginar",
-                "puan":"5",
-                "tarih":"20 Ekim 2022",
-                "yorumMetni":"İyi."
-            }
-        ]
+
+var hataGoster = function (res, durum) {
+    var mesaj;
+    if(hata.response.status==404){
+        mesaj="404, Sayfa Bulunamadı!";
     }
+    else{
+        mesaj=hata.response.status+"hatası";
+    }
+    res.status(hata.response.status);
+    res.render('error', {
+        "mesaj":mesaj
     });
+};
+
+    
+const mekanBilgisi= function(req, res) {
+    axios
+        .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + "/" + req.params.mekanid)
+        .then(function (response) {
+            detaySayfasiOlustur(res, response.data);
+        }
+        )
+        .catch(function (hata) {
+            hataGoster(res, hata);
+        });
+
 }
 const yorumEkle= function(req, res) {
     res.render('yorumekle', {'title': 'Yorum ekle'});
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var ctrlMekanlar = require('../controllers/mekanlar');
 var ctrlDigerleri = require('../controllers/digerleri');
 
-/* GET home page. */
 router.get('/', ctrlMekanlar.anaSayfa);
-router.get('/mekan', ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/:mekanid', ctrlMekanlar.mekanBilgisi);
 router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle);
 router.get('/hakkinda', ctrlDigerleri.hakkinda);
+
 
 module.exports = router;
