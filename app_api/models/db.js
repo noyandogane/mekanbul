9158f239610597791047de792d8da233d481b131
fromagery <ltmpinfo@gmail.com>
2022-12-03 15:35:42 +0300

    odev7

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,8 +1,8 @@
 var mongoose=require("mongoose");
 require("./mekansema");
-var dbURI = "mongodb+srv://noyan:1234@mekanbul.46wmrds.mongodb.net/mekanbul?retryWrites=true&w=majority"; 
+var dbURI = "mongodb+srv://noyan:f7UGhIbz2DWr3odn@mekanbul.wvpd89r.mongodb.net/mekanbul?retryWrites=true&w=majority"; 
 //var dbURI = "mongodb://localhost/mekanbul";
-mongoose.connect(dbURI, {useNewUrlParser:true});
+mongoose.connect(dbURI);
 function kapat(msg, callback) {
     mongoose.connection.close(function () {
         console.log(msg);
