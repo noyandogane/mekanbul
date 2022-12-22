d176e4f2db3d6d91b0e7b8233cfbf2a24b0dac9f
fromagery <ltmpinfo@gmail.com>
2022-12-22 11:15:49 +0300

    Update dogrulama.js

--- app_api/controllers/dogrulama.js
+++ app_api/controllers/dogrulama.js
@@ -1,17 +1,17 @@
 const passport = require('passport');
 const mongoose = require('mongoose');
 const Kullanici = mongoose.model('kullanici');
-const kayitOl = (req, res) => {
-    if (!req.body.adsoyad || !req.body.eposta || !req.body.sifre) {
-        return res.status(400).json({ "mesaj": "Lütfen tüm alanları doldurunuz." });
+const kayitOl = (req, res) => { //kayıt olma işlemleri
+    if (!req.body.adsoyad || !req.body.eposta || !req.body.sifre) { //kullanıcıdan gelen bilgilerin kontrolü
+        return res.status(400).json({ "mesaj": "Lütfen tüm alanları doldurunuz." }); //kullanıcıdan gelen bilgilerden biri eksikse hata mesajı döndür
     }
 
-    const kullanici = new Kullanici();
+    const kullanici = new Kullanici(); //kullanıcı modelinden yeni bir kullanıcı oluştur
     kullanici.adsoyad = req.body.adsoyad;
     kullanici.eposta = req.body.eposta;
     kullanici.sifreAyarla(req.body.sifre);
 
-    kullanici.save((hata) => {
+    kullanici.save((hata) => { //kullanıcıyı kaydet
         if (hata) {
             res.status(404).json(hata);
         } else {
@@ -21,19 +21,19 @@ const kayitOl = (req, res) => {
     });
 };
 
-const girisYap = (req, res) => {
+const girisYap = (req, res) => { //giriş yapma işlemleri
 
     if (!req.body.eposta || !req.body.sifre) {
         return res.status(400).json({ "mesaj": "Lütfen tüm alanları doldurunuz." });
     }
 
-    passport.authenticate('local', (err, kullanici, info) => {
+    passport.authenticate('local', (err, kullanici, info) => { //passport kullanarak giriş yapma işlemleri
         let token;
         if (err) {
             return res.status(404).json(err);
 
         }
-        if (kullanici) {
+        if (kullanici) { //kullanıcı varsa
             token = kullanici.tokenUret();
             res.status(200).json({ "token": token });
         } else {
@@ -43,7 +43,7 @@ const girisYap = (req, res) => {
 
 };
 
-module.exports = {
+module.exports = { //kayıt olma ve giriş yapma işlemlerini dışarıya aktarma
     kayitOl,
     girisYap
 
